5ffaffc0583ef15204efe41528723d7683d21c70
JoaoCavallin <joaopedrocavallin@gmail.com>
2026-03-11 21:16:04 -0300

    Adicionar arquivos de projeto.

---
+++ UI/Lista.xaml.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Repositorio;
+
+namespace UI
+{
+    /// <summary>
+    /// Lógica interna para Lista.xaml
+    /// </summary>
+    public partial class Lista : Window
+    {
+        public Lista()
+        {
+            InitializeComponent();
+            CarregarUsuarios();
+        }
+
+        private void CarregarUsuarios()
+        {
+            using (var context = new Context())
+            {
+          
+                dataGrid.ItemsSource = context.Usuarios.ToList();
+            }
+        }
+        private void btnVoltar(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ UI/Model/ProdutoModel.cs
@@ -0,0 +1,75 @@
+﻿using Dominio;
+using Dominio.Enum;
+using Repositorio;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UI.Model
+{
+    public class ProdutoModel
+    {
+        private ProdutoRepositorio _produtoRepositorio = new ProdutoRepositorio();
+
+        public async Task<Produto[]> ListarProdutos()
+        {
+            return await _produtoRepositorio.GetAllAsync();
+        }
+
+        public async void AdicionarProduto
+        (
+            string descricao,
+            UnidadeMedida unidadeDeMedida,
+            string codBarras,
+            decimal precoCusto,
+            decimal precoVenda,
+            bool ativo,
+            ProdutoGrupo produtoGrupo
+        )
+        {
+            Produto produto = new Produto(descricao, unidadeDeMedida, codBarras, precoCusto, precoVenda, DateTime.Now, ativo, produtoGrupo);
+
+            _produtoRepositorio.Add(produto);
+            await _produtoRepositorio.SaveChangesAsync();
+        }
+
+        public void ExcluirProduto(int id)
+        {
+            using (var context = new Context())
+            {
+                var produto = context.Produtos.FirstOrDefault(p => p.Id == id);
+
+                if (produto != null)
+                {
+                    context.Produtos.Remove(produto);
+                    context.SaveChanges();
+                    MessageBox.Show("Produto excluído com sucesso!");
+                }
+                else
+                {
+                    MessageBox.Show("Produto não encontrado.");
+                }
+            }
+        }
+        public async void EditarProduto
+        (
+            int Id,
+            string descricao,
+            UnidadeMedida unidadeDeMedida,
+            string codBarras,
+            decimal precoCusto,
+            decimal precoVenda,
+            bool ativo,
+            ProdutoGrupo produtoGrupo
+        )
+        {
+            Produto atualizarProduto = new Produto(Id, descricao, unidadeDeMedida, codBarras, precoCusto, precoVenda, DateTime.Now, ativo, produtoGrupo);
+
+            _produtoRepositorio.Update(atualizarProduto);
+            await _produtoRepositorio.SaveChangesAsync();
+        }
+
+
+    }
+}
